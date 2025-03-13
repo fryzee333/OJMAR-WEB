0c54ec75d75af273a7760be4f585d4d9d86bfae2
Samara Adrian <105038583+fryzee333@users.noreply.github.com>
2025-03-13 17:18:19 +0100

    newwwww

--- script.js
+++ script.js
@@ -65,4 +65,17 @@
             localStorage.setItem('horasTrabajadas', JSON.stringify(horasData));
             alert("Horas modificadas correctamente");
         }
-        
+
+        function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
